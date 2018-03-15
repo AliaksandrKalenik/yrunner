df39248d24f39247646fdab5f191747858b65108
Aliaksandr Kalenik <a.kalenik94@gmail.com>
2018-03-15 12:18:34 +0300

    added cors allow headers

--- core/middleware/cors_middleware.py
+++ core/middleware/cors_middleware.py
@@ -5,5 +5,5 @@ class CorsAllMiddleware(CommonMiddleware):
 
     def process_response(self, request, response):
         response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+        response["Access-Control-Allow-Headers"] = "*"
         return response
